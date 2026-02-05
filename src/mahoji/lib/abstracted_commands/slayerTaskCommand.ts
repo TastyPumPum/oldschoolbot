dce9a620422ec3230e3e83ecd4751f7338c30bc4
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2026-02-05 14:13:17 +0000

    Fix slayer auto-skip task assignment

--- src/mahoji/lib/abstracted_commands/slayerTaskCommand.ts
+++ src/mahoji/lib/abstracted_commands/slayerTaskCommand.ts
@@ -188,11 +188,12 @@ export async function slayerNewTaskCommand({
 		});
 		await user.statsUpdate({ [taskStreakKey]: 0 });
 
-		const newSlayerTask = await assignExtendedSlayerTask(user, slayerMaster);
+		const newSlayerTask = await assignExtendedSlayerTask(user, slayerMaster, rng);
 		const autoSkipResult = await autoSkipFromSkipList({
 			user,
 			master: slayerMaster,
-			initialTask: newSlayerTask
+			initialTask: newSlayerTask,
+			rng
 		});
 		const finalTask = autoSkipResult.finalTask;
 		const commonName = getCommonTaskName(finalTask.assignedTask.monster);
@@ -254,11 +255,12 @@ export async function slayerNewTaskCommand({
 		return resultMessage;
 	}
 
-	const newSlayerTask = await assignExtendedSlayerTask(user, slayerMaster);
+	const newSlayerTask = await assignExtendedSlayerTask(user, slayerMaster, rng);
 	const autoSkipResult = await autoSkipFromSkipList({
 		user,
 		master: slayerMaster,
-		initialTask: newSlayerTask
+		initialTask: newSlayerTask,
+		rng
 	});
 	const finalTask = autoSkipResult.finalTask;
 	const autoSkipMessage = formatAutoSkipMessage({
